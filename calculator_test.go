ebc482c98ec0b7163d23489f77eb7e8c89a5b777
Shikhar Gupta <shikhar.gupta120@gmail.com>
2024-11-15 16:27:26 -0500

    test code added by shikhar

--- calculator_test.go
+++ calculator_test.go
@@ -1,11 +1,35 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDivide(t *testing.T) {
+	result, err := Divide(6, 0)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Divide result:", result)
+	}
+	expected := 1
+	if result != expected {
+		t.Errorf("Divide(6, 0) will result %d, but we got %d", result, expected)
+	}
+}
+
+func TestSquare(t *testing.T) {
+	result := Square(2)
+	expected := 4
+	if result != expected {
+		t.Errorf("Square(2) will result %d, but we got %d", result, expected)
+	}
+}
 
 // Benchmark for Divide function with non-zero denominators
-func BenchmarkDivision(b *testing.B) {
+func BenchmarkDivide(b *testing.B) {
 	for h := 0; h < b.N; h++ {
-		Division(30, 7)
+		Divide(30, 7)
 	}
 }
 
